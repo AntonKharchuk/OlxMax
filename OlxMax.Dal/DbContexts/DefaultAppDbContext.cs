4c8cea60ad9d47469393c21bdbf4620f16131427
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-02-05 22:22:02 +0200

    Add Default Add Db Context

---
+++ OlxMax.Dal/DbContexts/DefaultAppDbContext.cs
@@ -0,0 +1,32 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+
+using OlxMax.Dal.Entities;
+
+using System;
+using System.Reflection.Metadata;
+
+namespace OlxMax.Dal.DbContexts
+{
+    public class DefaultAppDbContext : DbContext
+    {
+
+        public DbSet<Bet> Bets { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Auction> Auctions { get; set; }
+
+        public DefaultAppDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Auction>()
+                .HasMany(e => e.Bets)
+                .WithOne(e => e.Auction)
+                .HasForeignKey(e => e.AuctionId)
+                .IsRequired(true);
+        }
+
+    }
+}
