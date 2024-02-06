88d09dd8e0f0bf3ee51f107df81b7a2d6befb43a
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-02-06 07:53:03 +0200

    ExceptionHandlerMiddleware

--- OlxMax.Api/Program.cs
+++ OlxMax.Api/Program.cs
@@ -1,3 +1,5 @@
+using OlxMax.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -8,6 +10,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+app.UseMiddleware<ExceptionHandlerMiddleware>();
+
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
