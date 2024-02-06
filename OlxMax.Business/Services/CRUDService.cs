e4d8cc8378d39daeffd663dfaddffff4ab254f1c
Anton Kharchuk <Kharchuk.anton@gmail.com>
2024-02-06 07:22:05 +0200

    add base services

---
+++ OlxMax.Business/Services/CRUDService.cs
@@ -0,0 +1,44 @@
+﻿
+using OlxMax.Dal.Entity;
+using OlxMax.Dal.Repositories;
+
+namespace OlxMax.Business.Services
+{
+    public class CRUDService<T> : ICRUDService<T> where T : BaseEntity
+    {
+        protected readonly IGenericRepository<T> _repository;
+
+        public CRUDService(IGenericRepository<T> repository)
+        {
+            _repository = repository ?? throw new ArgumentNullException(nameof(repository));
+        }
+
+        public virtual async Task<IEnumerable<T>> GetAllItemsAsync(string include = "")
+        {
+            return await _repository.GetAllAsync(include);
+        }
+
+        public virtual async Task<T> GetItemByIdAsync(int id, string include = "")
+        {
+            return await _repository.GetByIdAsync(id, include);
+        }
+
+        public virtual async Task CreateItemAsync(T entity)
+        {
+            await _repository.AddAsync(entity);
+            await _repository.SaveAsync();
+        }
+
+        public virtual async Task UpdateItemAsync(T entity, int id)
+        {
+            await _repository.UpdateAsync(entity, id);
+            await _repository.SaveAsync();
+        }
+
+        public virtual async Task DeleteItemAsync(int id)
+        {
+            await _repository.DeleteAsync(id);
+            await _repository.SaveAsync();
+        }
+    }
+}
---
+++ OlxMax.Business/Services/ICRUDService.cs
@@ -0,0 +1,14 @@
+﻿
+using OlxMax.Dal.Entity;
+
+namespace OlxMax.Business.Services
+{
+    public interface ICRUDService<T> where T : BaseEntity
+    {
+        Task<IEnumerable<T>> GetAllItemsAsync(string include = "");
+        Task<T> GetItemByIdAsync(int id, string include = "");
+        Task CreateItemAsync(T entity);
+        Task UpdateItemAsync(T entity, int id);
+        Task DeleteItemAsync(int id);
+    }
+}
